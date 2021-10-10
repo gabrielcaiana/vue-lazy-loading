be489710a791ec84155fdff2af7ae4641c25f3e1
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-10 18:25:10 -0300

    config request api back-end

--- vue-search/src/views/Back-end.vue
+++ vue-search/src/views/Back-end.vue
@@ -8,7 +8,29 @@
 
 <script>
 import Products from '@/components/Products.vue';
+import { onMounted, ref } from 'vue';
 export default {
   components: { Products },
+  setup() {
+    const products = ref([]);
+
+    onMounted(async () => {
+      const response = await fetch('http://localhost:8000/api/products/backend');
+
+      try {
+        if (response.ok && response.status === 200) {
+          const content = await response.json();
+          products.value = content;
+        }
+      } catch (err) {
+        throw Error(err)
+      }
+    });
+
+    return {
+      products
+    }
+  },
 };
-</script>>
+</script>
+>
