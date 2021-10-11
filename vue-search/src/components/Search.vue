ed75d9690ec2efc739266126d9eee67a69690ace
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-10 22:09:45 -0300

    refactor search

--- vue-search/src/components/Search.vue
+++ vue-search/src/components/Search.vue
@@ -17,7 +17,7 @@
       duration-500
     "
   >
-    <div class="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
+    <div class="flex bg-gray-100 p-4 w-full space-x-4 rounded-lg">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-6 w-6 opacity-30"
@@ -38,50 +38,6 @@
         type="text"
         placeholder="Procurar produto..."
       />
-    </div>
-    <div
-      class="
-        flex
-        py-3
-        px-4
-        rounded-lg
-        text-gray-500
-        font-semibold
-        cursor-pointer
-      "
-    >
-      <span>Todas as categorias</span>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 9l-7 7-7-7"
-        />
-      </svg>
-    </div>
-    <div
-      class="
-        bg-green-400
-        py-3
-        px-5
-        text-white
-        font-semibold
-        rounded-lg
-        hover:shadow-lg
-        transition
-        duration-3000
-        cursor-pointer
-      "
-    >
-      <span>Pesquisar</span>
     </div>
   </div>
 </template>
